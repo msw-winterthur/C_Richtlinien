c7510b0dac74bea714543985a5e9bfa6f087c21b
Dario D <dario.duendar@gmail.com>
2019-05-23 10:05:43 +0200

    Funktion aus Prüfungsvorlage entfernt

--- Vorlagen/C-Pruefungsvorlage.c
+++ Vorlagen/C-Pruefungsvorlage.c
@@ -45,31 +45,11 @@
 #define INIT_LED    (DDRA = 0xFF)
 
 
-/*********************************************************************************\
-* invertiereLeds
-*
-* Kurzbeschreibung, was die Funktion macht (mehr in Stichworten, kein Aufsatz!).
-* Für diese Funktion würde die Beschreibung etwa so lauten:
-* Invertiert die Gewünschten Bits an den LEDs.
-*
-* Parameter:
-* zuInvertierendeBits = Bits die hier auf 1 sind, werden an den LEDs invertiert.
-*
-* Rückgabewert: Der neu angezeigte Wert wird zurückgegeben.
-*
-\*********************************************************************************/
-uint8_t invertiereLeds (uint8_t zuInvertierendeBits)
-{
-    LED = LED ^ zuInvertierendeBits;
-    return LED;
-}
 
 
 /* Hauptprogramm *****************************************************************/
 int main (void)
-{
-    uint8_t variable;
-    
+{    
     INIT_LED;
     
     //Endlosschlaufe
